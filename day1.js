c0eb498172829156cd83d41f7a09eb03f667968a
larts85 <lianel.sotolongo@reanswer.com.ar>
2021-12-02 20:00:51 -0300

    solutions day 1

--- day1.js
+++ day1.js
@@ -4,6 +4,14 @@ const { measurements } = require('./puzzle-inputs')
 const calculateAugmentedMeasurements = () => {
   let augmentedMeasurements = 0
 
+  for (let i = 1; i < measurements.length; i++) {
+    const currentMeasurement = measurements[i]
+    const previusMeasurement = measurements[i - 1]
+    if (currentMeasurement > previusMeasurement) {
+      augmentedMeasurements += 1
+    }
+  }
+
   return `There are ${`${augmentedMeasurements}`.green} augmented measurements.`
 }
 
@@ -11,6 +19,17 @@ const calculateAugmantesMeasurementsSumByThree = () => {
   let augmentedMeasurementsSum = 0
   let previusMeasurementsSum = 0
 
+  for (let i = 2; i < measurements.length; i++) {
+    const currentMeasurementsSum =
+      measurements[i] + measurements[i - 1] + measurements[i - 2]
+    if (
+      previusMeasurementsSum &&
+      previusMeasurementsSum < currentMeasurementsSum
+    ) {
+      augmentedMeasurementsSum += 1
+    }
+    previusMeasurementsSum = currentMeasurementsSum
+  }
   return `There are ${
     `${augmentedMeasurementsSum}`.green
   } augmented measurements sums.`
