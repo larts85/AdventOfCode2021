c3ce605b1de6a9d3168e1db25aec8f8896231c6f
larts85 <lianel.sotolongo@reanswer.com.ar>
2021-12-02 20:01:27 -0300

    solutions day 2

--- day2.js
+++ day2.js
@@ -1,11 +1,33 @@
 require('colors')
 const { submarineDirections } = require('./puzzle-inputs')
 
+const extractDirectionValue = (direction = '') => {
+  const [, value] = direction.split(' ')
+  return Number(value)
+}
 
 const calculateSubmarinePosition = () => {
   let horizontalPosition = 0
   let depth = 0
 
+  for (const direction of submarineDirections) {
+    switch (true) {
+      case /^forward/.test(direction):
+        horizontalPosition += extractDirectionValue(direction)
+        break
+
+      case /^down/.test(direction):
+        depth += extractDirectionValue(direction)
+        break
+
+      case /^up/.test(direction):
+        depth -= extractDirectionValue(direction)
+        break
+
+      default:
+        break
+    }
+  }
 
   return `The final direction is ${`${horizontalPosition * depth}`.green}.`
 }
@@ -15,6 +37,25 @@ const calculateEnhancedSubmarinePosition = () => {
   let depth = 0
   let aim = 0
 
+  for (const direction of submarineDirections) {
+    switch (true) {
+      case /^forward/.test(direction):
+        horizontalPosition += extractDirectionValue(direction)
+        depth += aim * extractDirectionValue(direction)
+        break
+
+      case /^down/.test(direction):
+        aim += extractDirectionValue(direction)
+        break
+
+      case /^up/.test(direction):
+        aim -= extractDirectionValue(direction)
+        break
+
+      default:
+        break
+    }
+  }
   return `The final direction is ${`${horizontalPosition * depth}`.green}.`
 }
 
